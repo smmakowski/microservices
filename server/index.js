6f8e1230d132a9f8e13295e16817d0db5cd21acb
smmakowski <makowski.sm@gmail.com>
2017-11-30 13:40:25 -0800

    start processDaete helper, add supertest

--- server/index.js
+++ server/index.js
@@ -1,14 +1,47 @@
 const http = require('http');
 const static = require('node-static');
-const PORT = 8000;
+const url = require('url');
+const PORT = process.env.PORT || 3000;
 const fileServer = new static.Server('./');
 
+function processDate(date) {
+  if (date.search(/\D/g)) { // if any letters, indicating possible natural date
+    console.log('NATURAL DATE')
+  } else {
+    console.log('ALL NUMBERS');
+  }
+}
+
 const server = http.createServer(function(req, res) {
+  const defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // in seonds
+  };
+
+  let statusCode = 200;
+  const pUrl = url.parse(req.url, true);
+  console.log(pUrl);
+  let dateQuery;
   let timeStamp = {
     naturalDate: null,
     unixDate: null
   }
+  if (req.method === 'GET' && pUrl.path === '/') {
+    statusCode = 200;
+  } else if (req.method === 'GET' && pUrl.path !== '/') {
+    statuscCode = 404;
+    dateQuery = pUrl.path.slice(1);
 
+  } else {
+    // throw error for POSTS/UPDATES/DELETES
+    statusCode = 404;
+  }
+  res.writeHead(statusCode);
+  console.log('STATUS CODE :' + res.statusCode);
+  console.log('HEADER : ' + res.headers);
+  res.end();
 });
 
 server.listen(PORT, function() {
--- test/serverEndpoints.js
+++ test/serverEndpoints.js
@@ -1,25 +1,67 @@
+const supertest = require('supertest');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const server = require('../server/index.js');
+// const sinon = require('sinon');
 const should = chai.should();
-
-chai.use(chaiHttp);
+const expect = chai.expect;
+const request = supertest(server);
 
 describe('API endpoints', function() {
   describe('Main Page', function(){
-    xit('should load index.html on / GET', function() {
+    xit('should send index.html on / GET', function() {
 
     });
   })
-  describe('Microservice', function() {
+  describe('Microservice valid endpoints', function() {
+    this.timeout(5000);
     it('should provide a 200 status on /* GET', function() {
+      request.get('/1450137600')
+      // .expect(404)
+      .end(function(err,res) {
+        expect(res.body).to.have.lengthOf(2);
+        done(err);
+      });
+    });
+    xit('should provide json response on /* GET', function() {
+      return chai.request(server)
+        .get('/1450137600')
+        .end(function(err, res) {
+          res.should.be.json;
+          done();
+        });
+    });
+    xit('json, when parsed should be an object on /* GET', function() {
+      return chai.request(server)
+        .get('/1450137600')
+        .end(function(err, res) {
+          res.body.should.be.a('number');
+          done();
+        });
+    });
+    xit('object should have correct unixTime on /* GET', function() {
+      return chai.request(server)
+        .get('/1450137600')
+        .end(function(err, res) {
+          console.log(res.body);
+          req.body.unixDate.should.equal(1450137600);
+          done();
+        });
+    });
+    it('json, when parsed should be an object on /* GET', function() {
       chai.request(server)
         .get('/1450137600')
         .end(function(err, res) {
-          console.log(res);
-          console.log(err);
-          res.should.have.status(200)
-          .done();
+          res.body.should.be.a('string');
+          done();
+        });
+    });
+    it('object should have properties with \'null\' values if invalid URL', function() {
+      chai.request(server)
+        .get('/1450137600')
+        .end(function(err, res) {
+          ob
+          done();
         });
     });
   });
