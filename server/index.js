c2b935cf958ed9aacddff5958183296075f06503
smmakowski <makowski.sm@gmail.com>
2017-11-30 19:49:50 -0800

    serve static file with fs; pass all tests for home page; basic tests 4 service

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const http = require('http');
 const static = require('node-static');
 const url = require('url');
 const PORT = process.env.PORT || 3000;
-const fileServer = new static.Server('./');
+// const fileServer = new static.Server('./');
+const fs = require('fs');
 
 function processDate(date) {
   if (date.search(/\D/g)) { // if any letters, indicating possible natural date
@@ -22,28 +23,34 @@ const server = http.createServer(function(req, res) {
 
   let statusCode = 200;
   const pUrl = url.parse(req.url, true);
-  console.log(pUrl);
+  // console.log(pUrl);
   let dateQuery;
 
   let timeStamp = {
     unix: null,
     natural: null
-  }
-  if (req.method === 'GET' && pUrl.path === '/') {
-    statusCode = 200;
-  } else if (req.method === 'GET' && pUrl.path !== '/') {
-    statuscCode = 200;
+  };
+
+  if (req.method === 'GET' && pUrl.path === '/') { // get Main page
+    fs.readFile('index.html', function(err, data) { // readfile and send
+      console.log(data);
+      res.setHeader('content-type', 'text/html');
+      res.writeHead(200);
+      res.end(data);
+    });
+  } else if (req.method === 'GET' && pUrl.path !== '/') { // if there is a queryString
     dateQuery = pUrl.path.slice(1);
+    res.writeHead(statusCode);
     res.end(JSON.stringify(timeStamp));
   } else {
     // throw error for POSTS/UPDATES/DELETES
     statusCode = 404;
   }
-  res.writeHead(statusCode);
-  console.log('STATUS CODE :' + res.statusCode);
-  console.log('HEADER : ' + res.headers);
-  console.log('BODY: ' + res.body);
-  res.end();
+  // res.writeHead(statusCode);
+  // console.log('STATUS CODE :' + res.statusCode);
+  // console.log('HEADER : ' + res.headers);
+  // console.log('BODY: ' + res.body);
+  // res.end();
 });
 
 server.listen(PORT, function() {
--- test/serverEndpoints.js
+++ test/serverEndpoints.js
@@ -3,35 +3,44 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('server', function() {
-  const query = 1450137600;
-
-  it('should respond to GET requests for /* 200 status code', function(done) {
-    request('http://127.0.0.1:3000/' + query, function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
+  describe('main page', function() {
+    it('should respond to GET requests or / 200 status code', function() {
+      request('http://127.0.0.1:3000/', function(error, response, body) {
+        const bodyString = body.toString();
+        expect(bodyString.search('<!DOCTYPE HTML>')).to.equal(true);
+        done();
+      });
     });
   });
-
-  it('should send back parsable stringified JSON', function(done) {
-    request('http://127.0.0.1:3000/' + query, function(error, response, body) {
-      expect(JSON.parse.bind(this, body)).to.not.throw();
-      done();
+  describe('timestamp microservice', function() {
+    const query = 1450137600;
+    it('should respond to GET requests for /* 200 status code', function(done) {
+      request('http://127.0.0.1:3000/' + query, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
-  });
 
-  it('should send back an object', function(done) {
-    request('http://127.0.0.1:3000/' + query, function(error, response, body) {
-      console.log(body);
-      var parsedBody = JSON.parse(body);
-      expect(parsedBody).to.be.an('object');
-      done();
+    it('should send back parsable stringified JSON', function(done) {
+      request('http://127.0.0.1:3000/' + query, function(error, response, body) {
+        expect(JSON.parse.bind(this, body)).to.not.throw();
+        done();
+      });
     });
-  });
-  xit('sent object should have properties "unix" and "natural"', function(done) {
-    request('http://127.0.0.1:3000/' + query, function(error, response, body) {
-      var parsedBody = JSON.parse(body);
-      expect(parsedBody).to.be.an('object');
-      done();
+
+    it('should send back an object', function(done) {
+      request('http://127.0.0.1:3000/' + query, function(error, response, body) {
+        var parsedBody = JSON.parse(body);
+        expect(parsedBody).to.be.an('object');
+        done();
+      });
+    });
+    it('sent object should have properties "unix" and "natural"', function(done) {
+      request('http://127.0.0.1:3000/' + query, function(error, response, body) {
+        var parsedBody = JSON.parse(body);
+        expect(parsedBody).to.have.all.keys('natural', 'unix');
+        done();
+      });
     });
   });
 });
